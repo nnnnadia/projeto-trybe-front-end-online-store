c7bdbd70925528374aaef061c08ac5125a64571c
nnnnadia <tristaonadia@gmail.com>
2022-05-29 12:33:43 -0300

    Estilização do menu de categorias.

--- src/components/CategorySelector.jsx
+++ src/components/CategorySelector.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ReactComponent as UnselectedIcon } from '../assets/icons/square.svg';
+import { ReactComponent as SelectedIcon } from '../assets/icons/x-square-fill.svg';
+import '../css/botao-categoria.css'
 
 export default class CategorySelector extends Component {
   render() {
-    const { name, id, onChange } = this.props;
+    const {
+      name,
+      id,
+      category,
+      onChange,
+    } = this.props;
     return (
-      <li>
+      <li className="botao-categoria">
         <label data-testid="category" htmlFor={ id }>
+          { category === id
+            ? <SelectedIcon />
+            : <UnselectedIcon />
+          }
           <input
             type="radio"
             id={ id }
             name="category"
             value={ id }
             onChange={ onChange }
+            hidden={ true }
           />
           { name }
         </label>
@@ -24,5 +37,6 @@ export default class CategorySelector extends Component {
 CategorySelector.propTypes = {
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
