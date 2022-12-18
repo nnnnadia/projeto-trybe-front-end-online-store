9d3d99dd6cb3f58eeb541f08a319fc147e369e25
nnnnadia <tristaonadia@gmail.com>
2022-12-18 11:03:11 -0300

    Updates

---
+++ precache-manifest.738591ce2d2fdb59dda36d7c05337912.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ac8666b181a9314784dab6ccd54e55",
+    "url": "/projeto-trybe-front-end-online-store/index.html"
+  },
+  {
+    "revision": "bb9d0cbc2b96628bd3c6",
+    "url": "/projeto-trybe-front-end-online-store/static/css/main.3d675f00.chunk.css"
+  },
+  {
+    "revision": "017c5422ccfeb941cd57",
+    "url": "/projeto-trybe-front-end-online-store/static/js/2.eb47b032.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/projeto-trybe-front-end-online-store/static/js/2.eb47b032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9d0cbc2b96628bd3c6",
+    "url": "/projeto-trybe-front-end-online-store/static/js/main.88af638c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/projeto-trybe-front-end-online-store/static/js/main.88af638c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198f2cd09c114ea849f4",
+    "url": "/projeto-trybe-front-end-online-store/static/js/runtime-main.b048ad72.js"
+  },
+  {
+    "revision": "51dbbb180c66343fe080f88e4b7960a4",
+    "url": "/projeto-trybe-front-end-online-store/static/media/cart-x.51dbbb18.svg"
+  },
+  {
+    "revision": "95703d9ad1427ba0a92c51e42a44dc94",
+    "url": "/projeto-trybe-front-end-online-store/static/media/cart4.95703d9a.svg"
+  },
+  {
+    "revision": "936501fbb2838fce4e8c14f59f89de6a",
+    "url": "/projeto-trybe-front-end-online-store/static/media/dash-square.936501fb.svg"
+  },
+  {
+    "revision": "e6668061ee810a2939e056eb36c177b3",
+    "url": "/projeto-trybe-front-end-online-store/static/media/plus-square.e6668061.svg"
+  },
+  {
+    "revision": "9b757d11a16b5daad3c1b3abce1239e5",
+    "url": "/projeto-trybe-front-end-online-store/static/media/search.9b757d11.svg"
+  },
+  {
+    "revision": "7e15cc48d866e0d117afc85810d3c20f",
+    "url": "/projeto-trybe-front-end-online-store/static/media/square.7e15cc48.svg"
+  },
+  {
+    "revision": "7495025507db8b8bca8bcab450c6dc8a",
+    "url": "/projeto-trybe-front-end-online-store/static/media/x-square-fill.74950255.svg"
+  }
+]);
